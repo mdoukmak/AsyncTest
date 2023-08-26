9aa67912a046ef01cac422dc125f9d86f4ad1fe6
Muhammad Doukmak <mkdoukmak@gmail.com>
2023-08-25 19:28:20 -0500

    Initial Commit

---
+++ AsyncTest/AsyncTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncTestApp.swift
+//  AsyncTest
+//
+//  Created by Muhammad Doukmak on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct AsyncTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
